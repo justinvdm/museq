0ca46592dbd93cecbbc7cacf448313f3c2e64330
justinvdm <justinvdm.0.1.0@gmail.com>
2014-11-07 22:21:16 +0200

    get the basics working for museq.pulse

--- src/pulse.js
+++ src/pulse.js
@@ -1,5 +1,27 @@
 museq.pulse = function() {
-  function pulse() {
+  function pulse(cps) {
+    var ticker
+    var out = sig()
+
+    var s = sig.map(sig(cps || 0.5), function(cps) {
+        if (ticker) clearInterval(ticker)
+        ticker = tick(out, cps)
+    })
+
+    sig.depend(s, out)
+    return out
+  }
+
+
+  function tick(s, cps) {
+    var i = 0
+
+    var id = setInterval(function() {
+      sig.push(s, ++i)
+    }, cps * 1000)
+
+    sig.push(s, i)
+    return id
   }
 
 
--- tests/museq.pulse.test.js
+++ tests/museq.pulse.test.js
@@ -1,5 +1,64 @@
 describe("museq.pulse", function() {
-  it("should exist", function() {
-    museq.pulse.should.exist
+  function capture(s) {
+    var values = []
+
+    sig.map(s, function(x) {
+      values.push(x)
+    })
+
+    return values
+  }
+
+  it("should push a new value each cycle", function(done) {
+    var results = capture(museq.pulse(0.1))
+    results.should.deep.equal([0])
+
+    setTimeout(function() {
+      results.should.deep.equal([0, 1])
+    }, 110)
+
+    setTimeout(function() {
+      results.should.deep.equal([0, 1, 2])
+    }, 210)
+
+    setTimeout(function() {
+      results.should.deep.equal([0, 1, 2, 3])
+      done()
+    }, 310)
+  })
+
+  it("should allow the cycles per second to be a signal", function(done) {
+    var cps = sig(0.1)
+
+    var results = capture(museq.pulse(cps))
+    results.should.deep.equal([0])
+
+    setTimeout(function() {
+      results.should.deep.equal([0, 1])
+    }, 110)
+
+    setTimeout(function() {
+      results.should.deep.equal([0, 1, 2])
+    }, 210)
+
+    setTimeout(function() {
+      results.should.deep.equal([0, 1, 2, 3])
+
+      sig.push(cps, 0.2)
+      results.should.deep.equal([0, 1, 2, 3, 0])
+    }, 310)
+
+    setTimeout(function() {
+      results.should.deep.equal([0, 1, 2, 3, 0, 1])
+    }, 520)
+
+    setTimeout(function() {
+      results.should.deep.equal([0, 1, 2, 3, 0, 1, 2])
+    }, 720)
+
+    setTimeout(function() {
+      results.should.deep.equal([0, 1, 2, 3, 0, 1, 2, 3])
+      done()
+    }, 920)
   })
 })
