6fb10d714bf86c66d6b44aba4eeefeeaa5916ef2
justinvdm <justinvdm.0.1.0@gmail.com>
2014-11-29 23:30:15 +0200

    add the basics for loop()

--- mu.js
+++ mu.js
@@ -10,7 +10,101 @@
 }(this, function(sig) {
 
 var mu = function() {
-  function loop() {
+  var all = sig.all,
+      reset = sig.reset,
+      cleanup = sig.cleanup,
+      resolve = sig.resolve,
+      resume = sig.resume,
+      ensure = sig.ensure,
+      watch = sig.watch,
+      map = sig.map
+
+  var globalOrigin = +(new Date())
+
+
+  function loop(v, interval, origin) {
+    parseLoopOpts(arguments)
+
+    return sig(function() {
+      var s = resume(sig())
+      var started = false
+      var curr
+
+      map(ensure(v), function update(x) {
+        curr = x
+        started = true
+      })
+
+      map(loopTick(interval, origin), function() {
+        if (started) put(s, curr)
+      })
+
+      return s
+    })
+  }
+
+
+  function loopTick() {
+    var args = arguments
+
+    return sig(function() {
+      var s = resume(sig())
+      var t
+
+      all(args, function(interval, origin) {
+        clear()
+        t = tick(interval, nextLoop(interval, origin))
+        watch(s, t)
+      })
+
+      cleanup(s, clear)
+      return s
+
+      function clear() {
+        if (t) reset(t)
+      }
+    })
+  }
+
+
+  function parseLoopOpts(args) {
+    var interval = args[1]
+    var origin = args[2]
+
+    if (interval && typeof interval == 'object') {
+      origin = interval.origin
+      interval = interval.interval
+    }
+
+    interval = deflt(interval, 2),
+    origin = +deflt(origin, globalOrigin)
+  }
+
+
+  function nextLoop(interval, origin) {
+    var now = +(new Date())
+    var i = Math.ceil((now - origin) / (interval * 1000))
+    var then = origin + (i * interval)
+    return then - now
+  }
+
+
+  function tick(interval, delay) {
+    return sig(function() {
+      var s = resume(sig())
+      var intervalId
+
+      var delayId = setTimeout(function() {
+        intervalId = setInterval(resolve, interval, s)
+      }, delay)
+
+      cleanup(s, function() {
+        clearTimeout(delayId)
+        clearInterval(intervalId)
+      })
+
+      return s
+    })
   }
 
 
@@ -23,6 +117,19 @@ var mu = function() {
 
 
   function run() {
+  }
+
+
+  function deflt(a, b) {
+    return exists(a)
+      ? a
+      : b
+  }
+
+
+  function exists(v) {
+    return typeof a != 'undefined'
+        && a !== null
   }
 
 
--- tests/mu.test.js
+++ tests/mu.test.js
@@ -1,5 +1,155 @@
 describe("mu", function() {
+  var val = sig.val,
+      put = sig.put
+
+  var testUtils = mu.testUtils,
+      fromNow = testUtils.fromNow,
+      timeCheck = testUtils.timeCheck
+
+  var at = timeCheck.at,
+      end = timeCheck.end
+
+  var loop = mu.loop
+
   describe(".loop", function() {
+    it("should loop the given value", function(done) {
+      vv(loop(23, 0.1, fromNow(-5)))
+        (timeCheck)
+        (at, 0, function(values) {
+          values.should.be.empty
+        })
+        (at, 110, function(values) {
+          values.should.deep.equal([23])
+        })
+        (at, 210, function(values) {
+          values.should.deep.equal([23, 23])
+        })
+        (at, 310, function(values) {
+          values.should.deep.equal([23, 23,23])
+        })
+        (at, 410, function(values) {
+          values.should.deep.equal([23, 23, 23, 23])
+        })
+        (at, 510, function(values) {
+          values.should.deep.equal([23, 23, 23, 23, 23])
+        })
+        (at, 610, function(values) {
+          values.should.deep.equal([23, 23, 23, 23, 23, 23])
+        })
+        (end, done)
+    })
+
+    it("should align loops to a given origin timestamp", function(done) {
+      vv(loop(23, 0.1, fromNow(-123)))
+        (timeCheck)
+        (at, 0, function(values) {
+          values.should.be.empty
+        })
+        (at, 10 + 123, function(values) {
+          values.should.deep.equal([23])
+        })
+        (at, 110 + 123, function(values) {
+          values.should.deep.equal([23, 23])
+        })
+        (at, 210 + 123, function(values) {
+          values.should.deep.equal([23, 23, 23])
+        })
+        (at, 310 + 123, function(values) {
+          values.should.deep.equal([23, 23, 23, 23])
+        })
+        (at, 410 + 123, function(values) {
+          values.should.deep.equal([23, 23, 23, 23, 23])
+        })
+        (end, done)
+    })
+
+    it("should allow value to be a signal", function(done) {
+      var v = val(23)
+
+      vv(loop(v, 0.1, fromNow(-5)))
+        (timeCheck)
+        (at, 0, function(values) {
+          values.should.be.empty
+        })
+        (at, 110, function(values) {
+          values.should.deep.equal([23])
+        })
+        (at, 210, function(values) {
+          values.should.deep.equal([23, 23])
+        })
+        (at, 310, function(values) {
+          values.should.deep.equal([23, 23, 23])
+          put(v, 3)
+        })
+        (at, 410, function(values) {
+          values.should.deep.equal([23, 23, 23, 3])
+        })
+        (at, 510, function(values) {
+          values.should.deep.equal([23, 23, 23, 3, 3])
+        })
+        (at, 610, function(values) {
+          values.should.deep.equal([23, 23, 23, 3, 3, 3])
+        })
+        (end, done)
+    })
+
+    it("should allow the interval to be a signal", function(done) {
+      var interval = val(0.1)
+
+      vv(loop(23, interval, fromNow(-5)))
+        (timeCheck)
+        (at, 0, function(values) {
+          values.should.be.empty
+        })
+        (at, 110, function(values) {
+          values.should.deep.equal([23])
+        })
+        (at, 210, function(values) {
+          values.should.deep.equal([23, 23])
+        })
+        (at, 310, function(values) {
+          values.should.deep.equal([23, 23, 23])
+          put(interval, 0.2)
+        })
+        (at, 520, function(values) {
+          values.should.deep.equal([23, 23, 23, 23])
+        })
+        (at, 720, function(values) {
+          values.should.deep.equal([23, 23, 23, 23, 23])
+        })
+        (at, 920, function(values) {
+          values.should.deep.equal([23, 23, 23, 23, 23, 23])
+        })
+        (end, done)
+    })
+
+    it("should allow the origin to be a signal", function(done) {
+      var origin = sig(fromNow(-123))
+      var originB = fromNow(-182)
+
+      vv(loop(23, 0.1, origin))
+        (timeCheck)
+        (at, 0, function(values) {
+          values.should.be.empty
+        })
+        (at, 10 + 123, function(values) {
+          values.should.deep.equal([23])
+        })
+        (at, 110 + 123, function(values) {
+          values.should.deep.equal([23, 23])
+        })
+        (at, 210 + 123, function(values) {
+          values.should.deep.equal([23, 23, 23])
+          put(origin, originB)
+        })
+        (at, 310 + 182, function(values) {
+          values.should.deep.equal([23, 23, 23, 23])
+        })
+        (at, 410 + 182, function(values) {
+          values.should.deep.equal([23, 23, 23, 23, 23])
+        })
+        (end, done)
+    })
   })
 
   describe(".seq", function() {
